d3c3167e2ce96f991a236585af9ce37b28bf194e
Rainer Hochecker <fernetmenta@online.de>
2011-03-26 09:14:37 +0100

    vdpau: rename bob deinterlacer in order to distinguish from other bob and add vdpau none in order to choose between yuv and rbg for progressive

--- xbmc/cores/dvdplayer/DVDCodecs/Video/VDPAU.cpp
+++ xbmc/cores/dvdplayer/DVDCodecs/Video/VDPAU.cpp
@@ -562,7 +562,8 @@ bool CVDPAU::Supports(EINTERLACEMETHOD method)
 
   if (hasVdpauGlInterop)
   {
-    if (method == VS_INTERLACEMETHOD_RENDER_BOB)
+    if (method == VS_INTERLACEMETHOD_RENDER_BOB
+    || method == VS_INTERLACEMETHOD_FORCE_VDPAU_NONE)
       return true;
   }
 
@@ -997,7 +998,7 @@ bool CVDPAU::ConfigOutputMethod(AVCodecContext *avctx, AVFrame *pFrame)
     m_bVdpauDeinterlacing = true;
   }
 
-  if (!m_bVdpauDeinterlacing && method != VS_INTERLACEMETHOD_VDPAU_NONE && hasVdpauGlInterop)
+  if (!m_bVdpauDeinterlacing && method != VS_INTERLACEMETHOD_FORCE_VDPAU_NONE && hasVdpauGlInterop)
   {
     if (m_vdpauOutputMethod == OUTPUT_GL_INTEROP_YUV)
       return true;
